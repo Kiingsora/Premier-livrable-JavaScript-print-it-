4474501c12bd86ebf11c54ee3c307a3bf6cb84f7
kiingsora <hichem.m83@gmail.com>
2026-01-16 09:22:15 +0100

    ajout logique java script

--- assets/script.js
+++ assets/script.js
@@ -1,35 +1,58 @@
 const slides = [
-	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
-]
+  {
+    image: "slide1.jpg",
+    tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
+  },
+  {
+    image: "slide2.jpg",
+    tagLine:
+      "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
+  },
+  {
+    image: "slide3.jpg",
+    tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
+  },
+  {
+    image: "slide4.png",
+    tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
+  },
+];
 
 let leftArrow = document.querySelector(".leftArrow");
 let rightArrow = document.querySelector(".rightArrow");
 
-leftArrow.addEventListener("click", ()=> {
-	console.log("fleche gauche");
-})
+leftArrow.addEventListener("click", () => {
+  console.log("fleche gauche");
+});
 
-rightArrow.addEventListener("click", ()=> {
-	console.log("fleche droite");
-})
+rightArrow.addEventListener("click", () => {
+  console.log("fleche droite");
+});
 
-let dots = document.querySelectorAll('.dots');
-for (let i = 0; i < dots.length; i++) {
-	const dot = dots[i]; 
+let imgs = document.querySelectorAll(".banner-img");
+let dots = document.querySelector(".dots");
+console.log(imgs);
+
+const banner = document.getElementById("banner");
+
+for (let i = 0; i < slides.length; i++) {
+
+  const slide = slides[i];
+  console.log(slides[i]);
+  
+//   let baliseImg = document.createElement("img");
+//   baliseImg.className = "banner-img";
+//   banner.appendChild(slide);
+
 }
+
+// slides.forEach((slide) => {
+// });
+
+// imgs.forEach((img) => {
+//   img = document.createElement("div");
+//   img.className = "dot";
+//   dots.appendChild(img);
+// });
+
 console.log(dots);
