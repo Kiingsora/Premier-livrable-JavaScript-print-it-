ed88dee7eeb8b26a822b60de02d07a492f4dbd6d
kiingsora <hichem.m83@gmail.com>
2026-01-28 09:58:25 +0100

    ajout de d'attribut alt pour chaque slide et ajout de dots qui se déplacent au clic des flèches

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,24 @@
-// Variables
+//----------------------- Variables
 const slides = [
   {
     image: "slide1.jpg",
     tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
+    alt: "background coloré jaune avec plusieurs dessins"
   },
   {
     image: "slide2.jpg",
-    tagLine:
-      "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
+    tagLine: "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
+    alt: "image d'un openspace"
   },
   {
     image: "slide3.jpg",
     tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
+    alt: "divers palette de couleur"
   },
   {
     image: "slide4.png",
     tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
+    alt: "image représentant une machine imprimant des journaux "
   },
 ];
 
@@ -26,10 +29,12 @@ let leftArrow = document.querySelector(".arrow_left");
 let rightArrow = document.querySelector(".arrow_right");
 let imgs = document.querySelector(".banner-img");
 let dots = document.querySelector(".dots");
+let arrayDots=[];
 
 const slideElement = [];
 
-// fonctions et boucles
+//----------------------- Fonctions and Loop
+
 slides.forEach((slide) => {
   const image = slide.image;
   const alt = slide.tagLine;
@@ -43,24 +48,38 @@ slides.forEach((slide) => {
   slideElement.push(slideShowed);
 
   createDots();
-  // dots.firstChild = "dot_selected" ;
-  // console.log(dots.firstChild);
+  arrayDots[0].classList.add ("dot_selected");
+
 });
 
 function createDots() {
   let dot = document.createElement("div");
   dot.className = "dot";
+  
+  arrayDots.push(dot)
   dots.appendChild(dot);
 }
 
-// event
+function updateSlideDisplay() {
+  slideElement.forEach((slide, index) => {
+    if (index === currentIndex) {
+      slide.style.display = "block";
+      arrayDots[index].classList.add ("dot_selected");
+    } else {
+      slide.style.display = "none"; 
+      arrayDots[index].classList.remove ("dot_selected");
+    }
+  });
+}
+
+//----------------------- Event
+
 rightArrow.addEventListener("click", () => {
   currentIndex++;
   if (currentIndex % 4 === 0) {
     currentIndex = 0;
   }
   updateSlideDisplay();
-  console.log(slideElement[currentIndex]);
 });
 
 leftArrow.addEventListener("click", () => {
@@ -72,13 +91,3 @@ leftArrow.addEventListener("click", () => {
   console.log(slideElement[currentIndex]);
 });
 
-function updateSlideDisplay() {
-  slideElement.forEach((slide, index) => {
-    if (index === currentIndex) {
-      slide.style.display = "block";
-      dots.classList = "dot_selected";
-    } else {
-      slide.style.display = "none"; // Masque les autres
-    }
-  });
-}
